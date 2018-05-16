6668637973cfe18f2a7ccf2e2692bb9ba31707c4
DESKTOP-8GB95EC\chris <z.chris.92@gmail.com>
2018-05-16 15:29:02 +0200

    fix js

--- public/js/site.js
+++ public/js/site.js
@@ -30,43 +30,48 @@ function replaceAll(str, find, replace) {
     return str.replace(new RegExp(find, 'g'), replace);
 }
 
-document.querySelector('#businessForm').addEventListener('submit', (e) => {
-    const formData = new FormData(e.target);
+if (document.querySelector('#businessForm')) {
+    document.querySelector('#businessForm').addEventListener('submit', (e) => {
+        const formData = new FormData(e.target);
 
-    fetch("https://hooks.zapier.com/hooks/catch/3305492/a6yzyr/", {
-        method: "POST",
-        body: formData
-    }).then(res => {
-        document.getElementById('businessForm').reset();
+        fetch("https://hooks.zapier.com/hooks/catch/3305492/a6yzyr/", {
+            method: "POST",
+            body: formData
+        }).then(res => {
+            document.getElementById('businessForm').reset();
+        });
+
+        success("Thank you! We will contact you shortly!");
+        e.preventDefault();
     });
+}
 
-    success("Thank you! We will contact you shortly!");
-    e.preventDefault();
-});
-
-      window.fbMessengerPlugins = window.fbMessengerPlugins || {
-        init: function () {
-          FB.init({
-            appId            : '1678638095724206',
-            autoLogAppEvents : true,
-            xfbml            : true,
-            version          : 'v2.10'
-          });
-        }, callable: []
-      };
-      window.fbAsyncInit = window.fbAsyncInit || function () {
-        window.fbMessengerPlugins.callable.forEach(function (item) { item(); });
-        window.fbMessengerPlugins.init();
-      };
-      setTimeout(function () {
-        (function (d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) { return; }
-          js = d.createElement(s);
-          js.id = id;
-          js.src = "//connect.facebook.net/en_US/sdk.js";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-      }, 0);
-      
-    
+window.fbMessengerPlugins = window.fbMessengerPlugins || {
+    init: function () {
+        FB.init({
+            appId: '1678638095724206',
+            autoLogAppEvents: true,
+            xfbml: true,
+            version: 'v2.10'
+        });
+    },
+    callable: []
+};
+window.fbAsyncInit = window.fbAsyncInit || function () {
+    window.fbMessengerPlugins.callable.forEach(function (item) {
+        item();
+    });
+    window.fbMessengerPlugins.init();
+};
+setTimeout(function () {
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+}, 0);
