a6612b04bff3068ad90610d27444d34f1cdef800
DESKTOP-8GB95EC\chris <z.chris.92@gmail.com>
2018-05-17 15:19:41 +0200

    burger pls buttons on carousel

--- public/js/site_19739.js
+++ public/js/site_19739.js
@@ -46,6 +46,32 @@ if (document.querySelector('#businessForm')) {
     });
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+  
+          // Get the target from the "data-target" attribute
+          var target = $el.dataset.target;
+          var $target = document.getElementById(target);
+  
+          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+          $el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
+
 if (document.getElementsByClassName('fb-customerchat')[0]) {
 
     var carousels = bulmaCarousel.attach();
